d86b3ad73401db060e02223d0e886b899b42a371
Mohamed-Elshesheny <mohamedelshesheny@std.mans.edu.eg>
2024-12-22 23:41:05 +0200

    cors update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const userRouter = require("./Routes/userRoute");
 const translateRouter = require("./Routes/translateRoute");
 const AppError = require("./utils/AppError");
@@ -7,6 +8,13 @@ const bodyParser = require("body-parser");
 const globalErrorHandler = require("./Middleware/errorMiddleware");
 
 const app = express();
+
+const corsOptions = {
+  origin: "*", // Allow all origins
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Allowed HTTP methods
+  allowedHeaders: "Content-Type,Authorization", // Allowed headers
+};
+app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
